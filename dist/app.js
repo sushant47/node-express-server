50ebcaf0c7d0aed8f2a28020139d437ac2ef5083
Sushant <Administrator@EEEPC0387.extentia05.local>
2019-01-28 18:04:33 +0530

    'changes'

--- dist/app.js
+++ dist/app.js
@@ -3,9 +3,19 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
 const bodyParser = require("body-parser");
 const crmRoutes_1 = require("./routes/crmRoutes");
+const mongoose = require("mongoose");
+// const userSchema = {
+//     "first_name": String,
+//     "last_name": String,
+//     "email": String,
+//     "gender": String,
+//     "ip_address": String,
+// };
 class App {
     constructor() {
+        this.mongoUrl = 'mongodb://localhost:27017/demo';
         this.routePrv = new crmRoutes_1.Routes();
+        this.mongoSetup();
         this.app = express();
         this.config();
         this.routePrv.routes(this.app);
@@ -16,6 +26,11 @@ class App {
         //support application/x-www-form-urlencoded post data
         this.app.use(bodyParser.urlencoded({ extended: false }));
     }
+    mongoSetup() {
+        mongoose.Promise = global.Promise;
+        mongoose.connect(this.mongoUrl);
+    }
 }
+// export const Users = mongoose.model('users', userSchema);
 exports.default = new App().app;
 //# sourceMappingURL=app.js.map
--- dist/controllers/crmController.js
+++ dist/controllers/crmController.js
@@ -2,7 +2,15 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose = require("mongoose");
 const crmModel_1 = require("../models/crmModel");
+const userSchema = {
+    "first_name": String,
+    "last_name": String,
+    "email": String,
+    "gender": String,
+    "ip_address": String,
+};
 const Contact = mongoose.model('Contact', crmModel_1.ContactSchema);
+const Users = mongoose.model('users', userSchema);
 class ContactController {
     getContacts(req, res) {
         Contact.find({}, (err, contact) => {
@@ -10,6 +18,42 @@ class ContactController {
                 res.send(err);
             }
             res.json(contact);
+        });
+    }
+    getUsers(req, res) {
+        Users.find({}, (err, contact) => {
+            if (err) {
+                res.send(err);
+            }
+            res.json(contact);
+        });
+    }
+    getUserPages(req, res) {
+        var pageNo = parseInt(req.query.pageNo);
+        var size = parseInt(req.query.size);
+        let query = {};
+        if (pageNo < 0 || pageNo === 0) {
+            res.send({ "error": true, "message": "invalid page number, should start with 1" });
+            return res;
+        }
+        query['skip'] = size * (pageNo - 1);
+        query['limit'] = size;
+        // Find some documents
+        Users.count({}, function (err, totalCount) {
+            if (err) {
+                res.send({ "error": true, "message": "Error fetching data" });
+            }
+            Users.find({}, {}, query, function (err, data) {
+                // Mongo command to fetch all data from collection.
+                if (err) {
+                    res.send({ "error": true, "message": "Error fetching data" });
+                }
+                else {
+                    var totalPages = Math.ceil(totalCount / size);
+                    res.send({ "error": false, "message": data, "pages": totalPages });
+                }
+                res;
+            });
         });
     }
 }
